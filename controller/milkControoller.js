6214c7eced0218456bd6e5644a58644a907d948e
farukhpatel <farukhpatel0804@gmail.com>
2021-07-04 15:15:11 +0530

    some changes

--- controller/milkControoller.js
+++ controller/milkControoller.js
@@ -1,40 +1,38 @@
-const Customer=require('../model/costomer');
-const Milk=require('./../model/milk');
+const Customer = require('../model/costomer');
+const Milk = require('./../model/milk');
 
-const milkControoller={
+const milkControoller = {
 
-  async addMilk(req,res){
-      const {customerId,liter,fat}=req.body;
-      let customer;
-      let data;
-  
-      try {
-        const find=await Customer.findOne({customerId});
-        if(find){
-          const milk=new Milk({
-            customerId,liter,fat
-          }); 
-          data=await milk.save();
-        } 
-        else{
-          return res.json({"User":"Not added in our dairy milk"});
-        }
-      } catch (error) {
-        return res.json({"Error":error});
-      }        
-         
-        
-      
-    
-      res.status(201).json(data);
-  },
-  async finding(req,res){
-    const customerId=req.params.customerId;
-    const milkData=await Milk.findOne({customerId});
-    if(!milkData){
-      return res.json({'message':'No customer found with this customer ID'});
+  async addMilk(req, res) {
+    const { customerId, liter, fat } = req.body;
+    let customer;
+    let data;
+
+    try {
+      const find = await Customer.findOne({ customerId });
+      if (!find) {
+        return res.json({ "User": "Not added in our dairy milk" });
+      }
+      const milk = new Milk({
+        customerId, liter, fat
+      });
+      data = await milk.save();
+    } catch (error) {
+      return res.json({ "Error": error });
     }
-    res.json({"milkHistory":milkData});
+
+
+
+
+    res.status(201).json(data);
+  },
+  async finding(req, res) {
+    const customerId = req.params.customerId;
+    const milkData = await Milk.findOne({ customerId });
+    if (!milkData) {
+      return res.json({ 'message': 'No customer found with this customer ID' });
+    }
+    res.json({ "milkHistory": milkData });
+  }
 }
-}
-module.exports=milkControoller;
+module.exports = milkControoller;
